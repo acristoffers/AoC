d8ce3b462a0121eecdc5045456d9adcce6d8c37c
Álan Crístoffer <acristoffers@startmail.com>
2022-12-03 11:04:23 +0100

    Day 3

---
+++ 2022/Day3/solve.el
@@ -0,0 +1,37 @@
+(defun read-file ()
+  (interactive)
+  (->> (split-string (buffer-substring-no-properties (point-min) (point-max)) "\n")
+       (-filter (lambda (x) (not (string-empty-p x))))))
+
+(defun solve ()
+  (interactive)
+  (->> (read-file)
+       (--map (let ((c (car (list-utils-uniq (list-utils-and
+                                              (string-to-list (substring it 0 (/ (length it) 2)))
+                                              (string-to-list (substring it (/ (length it) 2 -1))))))))
+                (cond ((and (<= c ?z) (>= c ?a))
+                       (- c 96))
+                      (t
+                       (+ 27 (- c ?A))))
+                ))
+       (-sum)
+       (message "Solution is %d")))
+
+(defun solve2 ()
+  (interactive)
+  (->> (read-file)
+       (--map-indexed (list it-index it))
+       (--partition-after-pred (= (mod (car it) 3) 2))
+       (--map (-flatten (-map #'-last-item it)))
+       (--map (-map #'string-to-list it))
+       (--map (list-utils-uniq (list-utils-and (list-utils-and
+                                                (-first-item it)
+                                                (-second-item it))
+                                               (-last-item it))))
+       (-flatten)
+       (--map (cond ((and (<= it ?z) (>= it ?a))
+                     (- it 96))
+                    (t
+                     (+ 27 (- it ?A)))))
+       (-sum)
+       (message "Solution is %d")))
