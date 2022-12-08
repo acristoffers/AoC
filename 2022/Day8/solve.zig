cca1c1371834cae72e27b801fc8e9682d8961202
Álan Crístoffer <acristoffers@startmail.com>
2022-12-08 21:07:01 +0100

    Fix: simplifies lineSize indexing.

--- 2022/Day8/solve.zig
+++ 2022/Day8/solve.zig
@@ -1,16 +1,15 @@
 const std = @import("std");
 
 const contents = @embedFile("input.txt");
-const lineSize = std.mem.indexOf(u8, contents, "\n").? + 1;
+const lineSize = std.mem.indexOf(u8, contents, "\n").?;
 
 inline fn at(i: usize, j: usize) u8 {
-    return contents[i * lineSize + j] - '0';
+    return contents[i * (lineSize + 1) + j] - '0';
 }
 
 fn isVisible(i: usize, j: usize) bool {
-    if (i == 0 or j == 0 or i == lineSize - 2 or j == lineSize - 2) return true;
+    if (i == 0 or j == 0 or i == lineSize - 1 or j == lineSize - 1) return true;
     const v = at(i, j);
-    const m = lineSize - 1;
     var blocks: bool = false;
     var k: usize = 0;
     while (k < i) : (k += 1) blocks = blocks or at(k, j) >= v;
@@ -21,17 +20,16 @@ fn isVisible(i: usize, j: usize) bool {
     if (!blocks) return true;
     k = i + 1;
     blocks = false;
-    while (k < m) : (k += 1) blocks = blocks or at(k, j) >= v;
+    while (k < lineSize) : (k += 1) blocks = blocks or at(k, j) >= v;
     if (!blocks) return true;
     k = j + 1;
     blocks = false;
-    while (k < m) : (k += 1) blocks = blocks or at(i, k) >= v;
+    while (k < lineSize) : (k += 1) blocks = blocks or at(i, k) >= v;
     return !blocks;
 }
 
 fn score(i: usize, j: usize) usize {
     const v = at(i, j);
-    const m = lineSize - 1;
     var leScore: usize = 0;
     var s: usize = 0;
     var k: usize = 0;
@@ -48,7 +46,7 @@ fn score(i: usize, j: usize) usize {
     leScore = s;
     s = 0;
     k = i + 1;
-    while (k < m) : (k += 1) {
+    while (k < lineSize) : (k += 1) {
         s += 1;
         if (at(k, j) >= v) break;
     }
@@ -67,7 +65,7 @@ fn score(i: usize, j: usize) usize {
     leScore *= s;
     s = 0;
     k = j + 1;
-    while (k < m) : (k += 1) {
+    while (k < lineSize) : (k += 1) {
         s += 1;
         if (at(i, k) >= v) break;
     }
@@ -78,9 +76,9 @@ fn score(i: usize, j: usize) usize {
 pub fn main() !void {
     var count: usize = 0;
     var i: usize = 0;
-    while (i < (lineSize - 1)) : (i += 1) {
+    while (i < lineSize) : (i += 1) {
         var j: usize = 0;
-        while (j < (lineSize - 1)) : (j += 1) {
+        while (j < lineSize) : (j += 1) {
             const v: usize = if (isVisible(i, j)) 1 else 0;
             if (v == 1) count += 1;
         }
@@ -89,9 +87,9 @@ pub fn main() !void {
 
     var leScore: usize = 0;
     i = 0;
-    while (i < (lineSize - 1)) : (i += 1) {
+    while (i < lineSize) : (i += 1) {
         var j: usize = 0;
-        while (j < (lineSize - 1)) : (j += 1) {
+        while (j < lineSize) : (j += 1) {
             leScore = @max(leScore, score(i, j));
         }
     }
